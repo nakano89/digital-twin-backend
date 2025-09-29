41b161fa7bee274e9016566c30ede37e608bd87c
aotk81 <abeyuma@gmail.com>
2025-09-29 12:07:03 +0900

    dtcl_api.pyのバグ修正

--- dtcl_api.py
+++ dtcl_api.py
@@ -16,10 +16,6 @@ import boto3
 from awscrt import auth, io, mqtt
 from awscrt.exceptions import AwsCrtError
 from awsiot import mqtt_connection_builder
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 # Refresh Token
 refresh_token = os.environ["DTCL_REFRESH_TOKEN"]  ########## 変更 ##########
